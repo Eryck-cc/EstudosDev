3fc2b6ebc1697756f97edcf3f6ae4230c45e390f
Eryck-cc <eryckkayck352@gmail.com>
2026-07-05 14:29:51 -0300

    task

--- EstudosC/Modulo3/Exercicio3.c
+++ EstudosC/Modulo3/Exercicio3.c
@@ -8,9 +8,9 @@ int main()
     printf("Digite um número inteiro: ");
     scanf("%d", &num);
 
-    while (contagem != num)
+    while (contagem != num + 1)
     {
-        printf("%d", contagem);
+        printf("%d\n", contagem);
         contagem ++;
     }
     return 0;
---
+++ EstudosC/Modulo3/Exercicio4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () 
+{
+    int num = 1;
+
+    do
+    {
+        printf("%d\n", num);
+        num ++;   /* code */
+    }
+    while (num < 6);
+    
+    return 0;
+}
